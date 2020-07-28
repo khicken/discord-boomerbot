67f6c570c270b822ea72ec5592d642a6e5f31d26
khicken <kalebkim1939@gmail.com>
2020-07-28 00:08:28 -0700

    began implementing file structure system for commands

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const Discord = require('discord.js'); // using discord js api
+const fs = require('fs'); // using filesystem lib
 const bot = new Discord.Client();
 
 const PREFIX = 'boomer ';
 var VERSION = '1.0.1';
 
-// boot up bot
+// using a filesystem to store individual commands
+
+
+// init boot
 bot.on('ready', () => {
     console.log(bot.user.username + ' is now online!');
     bot.user.setActivity('the world crumble', {
@@ -20,15 +24,17 @@ bot.on('message', message => {
             message.channel.send('Pong');
         break;
         case 'clr': case 'clear':
-            if(!args[1] || typeof args[1] != 'number') return message.reply('Invalid argument. For command help, type `%help`.');
+            if(!args[1]) return message.reply('Invalid argument. For command help, type `boomer help`.');
             message.channel.bulkDelete(args[1]);
         break;
         case 'qotd':
             message.channel.send('wip lol');
         break;
         case 'help':
-            message.channel.send("`I'm a boomer.\nTest, test, test\nI need to somehow put a list of helpful commands`");
+            message.channel.send("```I'm a boomer.\nTest, test, test\nI need to somehow put a list of helpful commands```");
         break;
+        case 'die':
+            message.channel.send("no u");
         default:
             // message.channel.send('not a command lol');
         break;
