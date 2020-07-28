f4f7e7942b23a4b46c8cf3bd41bb9b1a68633c99
khicken <kalebkim1939@gmail.com>
2020-07-28 14:25:59 -0700

    updated help.js

--- commands/help.js
+++ commands/help.js
@@ -1,15 +1,20 @@
-const { cmds, Discord, VERSION } = require("../main.js");
+const { Discord } = require("../main.js");
+var { cmds, VERSION } = require("../main.js");
 
 module.exports = {
     name: 'help',
     description: 'Display help commands',
     execute(message, args) {
+        // making a rlly long string that contains all commands with name and desc. . .
+        let cmdList = "";
+        for(let[name, description] of cmds) {
+            cmdList += name + ": " + description + "\n";
+        }
+        
         const embed = new Discord.MessageEmbed()
         .setTitle("Help Commands")
         .addField("Version", VERSION)
-        .addField("General", cmds.forEach(function(name, description) {
-            return name + " " + description;
-        }));
+        .addField("General", cmdList);
         message.channel.send(embed);
     }
 }
