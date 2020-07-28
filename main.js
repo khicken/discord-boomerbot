ae1000f04881d6e7ecd2b0f9b23b3ec020190941
khicken <kalebkim1939@gmail.com>
2020-07-27 23:20:18 -0700

    added prefix changing command (will remove or modify later), clear message command, and working on some other commands too

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const Discord = require('discord.js'); // using discord js api
 const bot = new Discord.Client();
 
-const PREFIX = '%';
+var PREFIX = '%';
 var VERSION = '1.0.1';
+var fr = new FileReader();
 
 // boot up bot
 bot.on('ready', () => {
@@ -12,13 +13,29 @@ bot.on('ready', () => {
     }).catch(console.error);
 });
 
-
 // commands
 bot.on('message', message => {
     let args = message.content.substring(PREFIX.length).split(' '); // split message's content into array of strings
     switch(args[0]) {
         case 'ping':
-            message.channel.send('pong!');
+            message.channel.send('Pong');
+        break;
+        case 'clr': case 'clear':
+            if(!args[1] || typeof args[1] != 'number') return message.reply('Invalid argument. For command help, type `%help`.');
+            message.channel.bulkDelete(args[1]);
+        break;
+        case 'qotd':
+            message.channel.send('wip lol');
+        break;
+        case 'change-prefix':
+            if(!args[1]) return message.reply('Specify an argument to change the prefix to please.');
+            PREFIX = args[1];
+        break;
+        case 'reset-prefix':
+            PREFIX = '%';
+        break;
+        case 'help':
+            message.channel.send("`I'm a boomer.\nTest, test, test\nI need to somehow put a list of helpful commands`");
         break;
     }
 });
