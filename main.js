5067b71c4bd1609bbfae1939ade40f7727490785
khicken <kalebkim1939@gmail.com>
2020-07-27 17:03:25 -0700

    added status and implemented 1 test command

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 const Discord = require('discord.js'); // using discord js api
 const bot = new Discord.Client();
 
+const PREFIX = '%';
+
+// init
 bot.on('ready', () => {
-    console.log(bot.user.username + " is now online");
+    console.log(bot.user.username + " is now online!");
 });
 bot.login(process.env.token);
+bot.user.setStatus("Being a boomer");
+
+// commands
+bot.on('message', message => {
+    let args = message.content.substring(PREFIX).split(' '); // split message's content into array of strings
+    switch(args[0]) {
+        case 'ping':
+            message.channel.send('pong!');
+            break;
+    }
+});
