fa4d3dfe2e11794c2c6f1a6cfe6a1ca03ec63300
khicken <kalebkim1939@gmail.com>
2020-07-29 11:28:19 -0700

    Revert "updated help.js" testing reverting
    This reverts commit f4f7e7942b23a4b46c8cf3bd41bb9b1a68633c99.

--- commands/help.js
+++ commands/help.js
@@ -1,20 +1,15 @@
-const { Discord } = require("../main.js");
-var { cmds, VERSION } = require("../main.js");
+const { cmds, Discord, VERSION } = require("../main.js");
 
 module.exports = {
     name: 'help',
     description: 'Display help commands',
     execute(message, args) {
-        // making a rlly long string that contains all commands with name and desc. . .
-        let cmdList = "";
-        for(let[name, description] of cmds) {
-            cmdList += name + ": " + description + "\n";
-        }
-        
         const embed = new Discord.MessageEmbed()
         .setTitle("Help Commands")
         .addField("Version", VERSION)
-        .addField("General", cmdList);
+        .addField("General", cmds.forEach(function(name, description) {
+            return name + " " + description;
+        }));
         message.channel.send(embed);
     }
 }
