7b780909bd7e005401f1408eb909722044a1f0da
khicken <kalebkim1939@gmail.com>
2020-08-04 19:33:08 -0700

    bug fixes and updated batch file (again)

--- push.bat
+++ push.bat
@@ -2,7 +2,6 @@ git add .
 set /p msg=Enter Commit Message(use "" if more than one word): 
 git commit -m %msg%
 git push
-pause
-set /p logCheck=Would you like to display heroku logs? (y/n) - 
-if %log%==y || %log%==yes heroku logs --tail -a discord-boomer-bot
+set /p logCheck=Would you like to display heroku logs? (y/n): 
+if %logCheck%==y || %logCheck%==yes heroku logs --tail -a discord-boomer-bot
 pause
